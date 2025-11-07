0a7374ff5bddb3a9751be93f2e84faf6fd7a443a
Aditya bisht <adityabisht.cx@gmail.com>
2025-11-07 12:09:24 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.03%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int climbStairs(int n)
+   {
+        if(n==1)
+        {
+            return 1;
+        }
+        else if(n==2)
+        {
+            return 2;
+        }
+       int first=1;
+       int second=2;
+       int third;
+       for(int i=3;i<=n;i++)
+       {
+        third=first+second;
+        first=second;
+        second=third;
+       }
+       return third;
+    }
+    void main()
+    {
+        cout<<climbStairs(43);
+    }
+};
